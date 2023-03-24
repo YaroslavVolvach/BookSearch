1c0bd9a716425baa1aed50cb860a7fb2d73d3497
Yaroslav Volvach <volvachya@gmail.com>
2023-03-24 17:11:51 -0400

    k

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -8,7 +8,7 @@ import { removeBookId } from '../utils/localStorage';
 const SavedBooks = () => {
   // eslint-disable-next-line
   const {loading, data} = useQuery(GET_ME);
-  console.log('GET: ', data);
+  
   const [removeBook] = useMutation(REMOVE_BOOK);
   // use this to determine if `useEffect()` hook needs to run again
 
--- server/server.js
+++ server/server.js
@@ -2,13 +2,13 @@ const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
 const { authMiddleware } = require("./utils/auth");
-const { ApolloServer } = require("@apollo/server");
+const { ApolloServer } = require("apollo-server-express");
 const { expressMiddleware } = require("@apollo/server/express4");
 const { typeDefs, resolvers } = require("./schemas");
-const cors = require('cors')
 
-
-const { ApolloServerPluginLandingPageGraphQLPlayground } = require('@apollo/server-plugin-landing-page-graphql-playground')
+const {
+  ApolloServerPluginDrainHttpServer,
+} = require("@apollo/server/plugin/drainHttpServer");
 
 const http = require("http");
 
@@ -16,28 +16,26 @@ const app = express();
 const httpServer = http.createServer(app);
 const PORT = process.env.PORT || 3001;
 
-
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
   context: authMiddleware,
 });
 
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json())
+app.use(express.json());
 
 // if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production'){
+if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
 //app.use(routes);
 
-async function startApolloServer(typeDefs, resolvers) {
+const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
-  app.use(cors({ methods: ['GET', 'POST', 'OPTIONS'], credentials: true, maxAge: 600, origin: ['*'],})),
-  app.use(cors(), expressMiddleware(server));
+  server.applyMiddleware({ app });
+
   db.once("open", () => {
     httpServer.listen(PORT, () => {
       console.log(`API server running on port ${PORT}!`);
